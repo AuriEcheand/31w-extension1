f0917afdad524415a5e5b2a5eca532bb5e580f9e
AuriEcheand <aecheand@gmail.com>
2022-06-20 14:16:31 -0400

    s6-c11 ouverture et fermeturede la boite modale et contenu

--- script/message.js
+++ script/message.js
@@ -1,33 +1,40 @@
 (function(){
+    let elmBody = document.querySelector('body')
     let elmBoite__modale
     let elmBoite__x
+    let elmBoite__contenu
+
+     //document.querySelector('.boite__modale')
+     let elmOK = document.querySelector('#mon_bouton')
 
     function creer_boite_modale(){
-        let elmBoite__modale = document.createElement('div')
+        elmBoite__modale = document.createElement('div')
+        elmBoite__contenu = document.createElement('div')
+        elmBoite__contenu.classList.add('elmBoite__contenu')
         elmBoite__x = document.createElement('button')
+        elmBoite__modale.appendChild(elmBoite__contenu)
         elmBoite__modale.appendChild(elmBoite__x)
         elmBoite__x.classList.add('boite__x')
         elmBoite__x.innerHTML = "X"
         elmBoite__modale.classList.add('boite__modale')
-        let elmBody = document.querySelector('body')
         elmBody.appendChild(elmBoite__modale);
+        elmBoite__contenu.innerHTML = "ceci est un test"
+    }
+    function detruire_boite_modale(){
+        elmBoite__modale.removeChild(elmBoite__x)
+        elmBody.removeChild(elmBoite__modale)
     }
   
     creer_boite_modale()
-
-    //document.querySelector('.boite__modale')
-    let elmOK = document.querySelector('#mon_bouton')
-
     elmOK.addEventListener('mousedown', function(){
         console.log('Bravo le script fonctionne!')
-        elmBoite__modale.classList('boite__modale__ouvrir')
-
     })
 
-    elmBoite__X.addEventListener('mousedown', function() {
+    elmBoite__X.addEventListener('mousedown', function(){
+
         elmBoite__modale.classList.remove('boite__modale__ouvrir')
         console.log(elmBoite__modale.classList)
-    
 
     })
+
 })()
